9eaaaf328a0814caadde07a1445bcc2f740c63af
deanishe <deanishe@deanishe.net>
2013-11-03 20:03:44 +0100

    Move logging to own module

---
+++ src/log.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Copyright © 2013 deanishe@deanishe.net.
+#
+# MIT Licence. See http://opensource.org/licenses/MIT
+#
+# Created on 2013-11-03
+#
+
+"""
+"""
+
+from __future__ import print_function
+
+import os
+import alfred
+import logging
+
+LOGGING = True  # to log or not to log
+
+LOGFILE = os.path.join(alfred.work(True), u'debug.log')
+
+_handler = None
+
+def logger(name=u''):
+    global _handler
+    if not _handler:
+        if LOGGING:
+            _handler = logging.FileHandler(filename=LOGFILE,
+                                           encoding=u'utf-8',
+                                           delay=True)
+        else:
+            _handler = logging.NullHandler()
+    log = logging.getLogger(name)
+    log.addHandler(_handler)
+    log.setLevel(logging.DEBUG)
+    return log
